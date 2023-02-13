f02f3fd69a882d9f201b6c8dc1455c4332a7cdda
OUMAR3 <oumaralitori1@gmail.com>
2023-02-13 12:58:18 +0100

    ajouter et modeifaction sur la partie de menu

--- restaurantHananaApp/views.py
+++ restaurantHananaApp/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    ctx={"title":"Restaurant Hanana "}
+    a = list(range(4))
+    ctx={"title":"Restaurant Hanana ","a":a}
+    
     return render(request,'base/index.html',ctx)
 def pizza(request):
     return HttpResponse('Hello Pizza')
